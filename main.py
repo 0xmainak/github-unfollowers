8b946f0acf75f50cfe313016cf01eea8de54e2f4
0xmainak <mainakpanda30@gmail.com>
2025-02-26 19:00:05 +0530

    added requirements.txt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.functions import GithubAPI
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
 
 GITHUB_USERNAME = os.getenv('GITHUB_USERNAME')
@@ -15,6 +16,8 @@ if __name__ == "__main__":
     
     if following is not None and followers is not None:
         non_followers = following - followers
+
+        # print(non_followers)
         
         print(f"Unfollowing {len(non_followers)} users who don't follow back...")
         for user in non_followers:
